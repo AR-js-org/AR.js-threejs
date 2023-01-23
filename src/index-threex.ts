049a1c831d423c208606f5875020d41809eade78
Walter Perdan <github@kalwaltart.it>
2023-01-23 22:16:33 +0100

    exporting the two namespaces: ARjs and THREEx

--- src/index-threex.ts
+++ src/index-threex.ts
@@ -1,11 +1,16 @@
-import { ArToolkitContext } from "./ArToolkitContext";
-import { ArToolkitProfile } from "./ArToolkitProfile";
-import { ArToolkitSource } from "./ArToolkitSource";
-import { ArMarkerControls } from "./ArMarkerControls";
+import { ArToolkitContext as _ArToolkitContext } from "./ArToolkitContext";
+import { ArToolkitProfile as _ArtoolkitProfile } from "./ArToolkitProfile";
+import { ArToolkitSource as _ArToolkitSource } from "./ArToolkitSource";
+import { ArMarkerControls as _ArMarkerControls  } from "./ArMarkerControls";
+import { Utils as _Utils } from "./new-api/Utils";
 
-export {
-    ArToolkitContext,
-    ArToolkitProfile,
-    ArToolkitSource,
-    ArMarkerControls
-  };
+export namespace THREEx {
+  export const ArToolkitContext = _ArToolkitContext;
+  export const ArToolkitProfile = _ArtoolkitProfile;
+  export const ArToolkitSource  = _ArToolkitSource;
+  export const ArMarkerControls = _ArMarkerControls ;
+};
+
+export namespace ARjs {
+  export const Utils = _Utils;
+ };
