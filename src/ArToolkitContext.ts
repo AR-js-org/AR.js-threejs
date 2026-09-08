620cafbe8f203e8fc338969f2c80686fb6b03a05
Walter Perdan <github@kalwaltart.it>
2026-09-08 23:21:29 +0200

    bump version 3.4.3-threejs-0.3.3

--- src/ArToolkitContext.ts
+++ src/ArToolkitContext.ts
@@ -114,7 +114,7 @@ export class ArToolkitContext implements IArToolkitContext {
     removeEventListener = EventDispatcher.prototype.removeEventListener;
 
     static baseURL = "https://ar-js-org.github.io/AR.js/three.js/";
-    static REVISION = "3.4.3-threejs-0.3.2";
+    static REVISION = "3.4.3-threejs-0.3.3";
 
     /**
      * Create a default Camera 
